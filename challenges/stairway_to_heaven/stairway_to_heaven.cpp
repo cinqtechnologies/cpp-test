d017a935a6494d080168c4d4fb9057b4d16538f8
Quenzo <gabriel.quenzo@gmail.com>
2016-12-08 22:46:28 -0200

    Update stairway_to_heaven.cpp

--- challenges/stairway_to_heaven/stairway_to_heaven.cpp
+++ challenges/stairway_to_heaven/stairway_to_heaven.cpp
@@ -4,5 +4,14 @@
 
 int main(int argc, char** argv)
 {
+	int nLines = 0;
+	std::cin >> nLines;
+	std::string step = "";
+	for (int n = 0; n < nLines; n++)
+	{
+		step += '#';
+		std::cout << std::setfill(' ') << std::setw(nLines);
+		std::cout << step << std::endl;
+	}
 	return 0;
 }
