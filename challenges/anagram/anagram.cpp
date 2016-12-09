b0c31d05885fb7cfe9307f03bd7f64cd0d176dd8
Quenzo <gabriel.quenzo@gmail.com>
2016-12-08 22:39:10 -0200

    Update anagram.cpp
    Add functionalities described in readme.txt.

--- challenges/anagram/anagram.cpp
+++ challenges/anagram/anagram.cpp
@@ -2,7 +2,46 @@
 #include <algorithm>
 #include <string>
 
+bool areAnagram(std::string a, std::string b)
+{
+	if (a.length() == b.length())
+	{
+		std::sort(a.begin(), a.end());
+		std::sort(b.begin(), b.end());
+		return a == b;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 int main(int argc, char** argv)
 {
+	int linesToRead;
+	std::cin >> linesToRead;
+	std::cin.ignore(); 
+	if (linesToRead > 0)
+	{
+		std::string firstWord, secondWord;
+		while (linesToRead--)
+		{
+			std::getline(std::cin, firstWord, ' ');
+			std::getline(std::cin, secondWord);
+			if (areAnagram(firstWord, secondWord))
+			{
+				std::cout << firstWord << " is an anagram of " << secondWord << std::endl;
+			}
+			else
+			{
+				std::cout << firstWord << " is not an anagram of " << secondWord << std::endl;
+			}
+		}
+	}
+	else
+	{
+		std::cout << "Invalid input";
+	}
+
 	return 0;
 }
