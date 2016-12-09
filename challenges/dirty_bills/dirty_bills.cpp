a19fb4d97ea3a3a0ead9d901b2e2eb308df5a20e
Quenzo <gabriel.quenzo@gmail.com>
2016-12-08 22:44:04 -0200

    Update dirty_bills.cpp

--- challenges/dirty_bills/dirty_bills.cpp
+++ challenges/dirty_bills/dirty_bills.cpp
@@ -3,5 +3,18 @@
 
 int main(int argc, char** argv)
 {
+	unsigned numLines;
+	int dirtyBills = 0;
+	std::string rawInput;
+	std::cin >> numLines;
+	while (numLines--)
+	{
+		std::cin >> rawInput;
+		if (rawInput.find("dirty") != std::string::npos)
+		{
+			dirtyBills += std::stoi(rawInput);
+		}
+	}
+	std::cout << "There are " << dirtyBills << " dirty bills.\n";
 	return 0;
 }
