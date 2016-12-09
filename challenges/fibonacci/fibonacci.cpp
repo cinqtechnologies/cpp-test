35b791df3d0d269c6bb7e434817950bad3412bee
Quenzo <gabriel.quenzo@gmail.com>
2016-12-08 22:44:26 -0200

    Update fibonacci.cpp

--- challenges/fibonacci/fibonacci.cpp
+++ challenges/fibonacci/fibonacci.cpp
@@ -2,5 +2,21 @@
 
 int main(int argc, char** argv)
 {
+	unsigned fiboX, fiboLastLast = 0, fiboLast = 1, fiboCurrent = 0;
+	std::cin >> fiboX;
+	for (unsigned reps = 0; reps <= fiboX; reps++)
+	{
+		if (reps <= 1)
+		{
+			fiboCurrent = reps;
+		}
+		else
+		{
+			fiboCurrent = fiboLast + fiboLastLast;
+			fiboLastLast = fiboLast;
+			fiboLast = fiboCurrent;
+		}
+	}
+	std::cout << fiboCurrent;
 	return 0;
 }
