2522e90fbb0dd0710731c4384e3df5b8be48fd23
Quenzo <gabriel.quenzo@gmail.com>
2016-12-08 22:44:49 -0200

    Update functional_anarchy.cpp

--- challenges/functional_anarchy/functional_anarchy.cpp
+++ challenges/functional_anarchy/functional_anarchy.cpp
@@ -6,5 +6,18 @@
 
 int main(int argc, char** argv)
 {
+	int numLines, inputNum;
+	std::vector<int> myVec;
+	std::cin >> numLines;
+	for (int reps = 0; reps < numLines; reps++)
+	{
+		std::cin >> inputNum;
+		myVec.push_back(inputNum);
+	}
+	std::sort(myVec.begin(), myVec.end());
+	for (std::vector<int>::iterator it = myVec.begin(); it != myVec.end(); it++)
+	{
+		std::cout << *it << "\n";
+	}
 	return 0;
 }
