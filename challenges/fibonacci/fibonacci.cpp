6af4102c91872e3a3da4eca233a4ca7b2ac34187
Luis Copetti <lhcopetti@gmail.com>
2017-06-26 22:47:36 -0300

    INT: Implementação do Fibonacci

--- challenges/fibonacci/fibonacci.cpp
+++ challenges/fibonacci/fibonacci.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+long long fibonacci(size_t fibonacciIndex)
+{
+	if (fibonacciIndex < 2)
+		return fibonacciIndex;
+
+	long long current = 1;
+	long long previous = 0;
+	for (int i = 1; i < fibonacciIndex; i++)
+	{
+		long long sum = current + previous;
+		previous = current;
+		current = sum;
+	}
+
+	return current;
+}
+
 int main(int argc, char** argv)
 {
-	return 0;
+	int fibonacciIndex;
+	std::cin >> fibonacciIndex;
+	std::cout << fibonacci(fibonacciIndex);
 }
