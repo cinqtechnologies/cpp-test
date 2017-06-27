12f10906489d0a8e6b8ebd032d01507ab7f95986
Luis Copetti <lhcopetti@gmail.com>
2017-06-26 22:56:08 -0300

    INT: Implementação do problema de Deadly Diamond

--- challenges/diamonds_in_the_sky/diamonds_in_the_sky.cpp
+++ challenges/diamonds_in_the_sky/diamonds_in_the_sky.cpp
@@ -21,7 +21,7 @@ private:
 	int m_stars;
 };
 
-class BlueSky : public Sky
+class BlueSky : public virtual Sky
 {
 public:
 	BlueSky(int stars = 0)
@@ -29,7 +29,7 @@ public:
 	{}
 };
 
-class NightSky : public Sky
+class NightSky : public virtual Sky
 {
 public:
 	NightSky(int stars = 0)
