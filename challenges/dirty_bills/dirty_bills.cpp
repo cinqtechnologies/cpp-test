cff687c739368b84378ee5ede5506513a73b7e8e
Luis Copetti <lhcopetti@gmail.com>
2017-06-26 23:10:00 -0300

    INT: Implementação do exercício 'Dirty Bills'

--- challenges/dirty_bills/dirty_bills.cpp
+++ challenges/dirty_bills/dirty_bills.cpp
@@ -3,5 +3,26 @@
 
 int main(int argc, char** argv)
 {
-	return 0;
+	int numberOfBills;
+	std::cin >> numberOfBills;
+
+	int sumOfDirtyBills = 0;
+	int number;
+	std::string billType;
+	while(numberOfBills-- > 0)
+	{
+		std::cin >> number;
+		std::cin >> billType;
+
+		if (billType == "dirty")
+			sumOfDirtyBills += number;
+	}
+
+
+	if (sumOfDirtyBills > 0)
+		std::cout << "There are " << sumOfDirtyBills << " dirty bills.";
+	else
+		std::cout << "There are no dirty bills.";
+
+	std::cout << std::endl;
 }
