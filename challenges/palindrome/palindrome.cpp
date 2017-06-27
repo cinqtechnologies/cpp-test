6710af46171cde1833d0465d0c4af5154b7a0b22
Luis Copetti <lhcopetti@gmail.com>
2017-06-26 23:01:30 -0300

    INT: Implementação do teste do Palindromo

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 int main(int argc, char** argv)
 {
+	int numberOfWords;
+	std::cin >> numberOfWords;
+
+	std::string word;
+	std::string reversedWord;
+	while(numberOfWords-- > 0)
+	{
+		std::cin >> word;
+		reversedWord = word;
+		std::reverse(reversedWord.begin(), reversedWord.end());
+		bool isPalindrome = word == reversedWord;
+		std::cout << word << "is " << (isPalindrome ? "" : "not ") << " palindrome" << std::endl;
+	}
+
 	return 0;
 }
