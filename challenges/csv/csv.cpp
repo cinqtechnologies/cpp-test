9e3f4de9069ef2803540b11290ce8d9dc8bb6943
Jorge <jorgezimm@gmail.com>
2018-08-20 17:34:28 -0300

    temporario

--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -6,5 +6,23 @@
 
 int main(int argc, char** argv)
 {
+	int n;
+	std::cin >> n;
+	std::cin.get();
+
+	for (int i = 0; i < n; i++) {
+		std::string line;
+		getline(std::cin, line);
+		std::vector<std::string> fields;
+		std::string field;
+		std::stringstream stream(line);
+		while(std::getline(stream, field, ','))
+		{
+			fields.push_back(field);
+		}
+		
+		std::cout << fields[0] << " is " << fields[4] << " years old and lives in " << fields[2] << ", " << fields[1] << "." << std::endl;
+	}
+
 	return 0;
 }
--- challenges/dirty_bills/dirty_bills.cpp
+++ challenges/dirty_bills/dirty_bills.cpp
@@ -3,5 +3,25 @@
 
 int main(int argc, char** argv)
 {
+	int n, dirty = 0;
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int number;
+		std::string dirtyOrClean;
+		std::cin >> number;
+		std::cin >> dirtyOrClean;
+		if (dirtyOrClean.compare("dirty") == 0) {
+			dirty += number;
+		}
+	}
+
+	if (dirty == 0) {
+		std::cout << "There are no dirty bills." << std::endl;
+	} else {
+		std::cout << "There are " << dirty << " dirty bills." << std::endl;
+	}	
+	
+
 	return 0;
 }
