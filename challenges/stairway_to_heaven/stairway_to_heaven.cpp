089b9717839dac89563197701be8c2d600218baa
Rafael Henrique Tibães <r@fael.nl>
2018-09-11 16:33:27 -0300

    stairway_to_heaven

--- challenges/stairway_to_heaven/stairway_to_heaven.cpp
+++ challenges/stairway_to_heaven/stairway_to_heaven.cpp
@@ -1,8 +1,20 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
-#include <iomanip>
 
-int main(int argc, char** argv)
-{
-	return 0;
+using namespace std;
+
+int main(int argc, char **argv) {
+  int N = 0;
+  cin >> N;
+  if (N <= 0) {
+    cout << "Error." << endl;
+    return 0;
+  }
+  string line(N, ' ');
+  for (auto i = line.rbegin(); i != line.rend(); ++i) {
+    *i = '#';
+    cout << line << endl;
+  }
+  return 0;
 }
