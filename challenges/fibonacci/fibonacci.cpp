c89d6ce4bb006863800bf136c3a43ab404a81fd1
Rafael Henrique Tibães <r@fael.nl>
2018-09-11 14:57:35 -0300

    fibonacci

--- challenges/fibonacci/fibonacci.cpp
+++ challenges/fibonacci/fibonacci.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <tuple>
 
-int main(int argc, char** argv)
-{
-	return 0;
+// There are lots of algorithms for fibonacci
+// The chosen one is O(log n) and is better for high N
+// However, dynamic programming is faster for low N
+
+using namespace std;
+
+using tuplell = tuple<uint64_t, uint64_t>;
+
+tuplell fib(const uint64_t n) {
+  if (n <= 0) {
+    return tuplell(0, 1);
+  }
+  const auto [a, b] = fib(n >> 1);
+  const auto c = a * ((b << 1) - a);
+  const auto d = (a * a) + (b * b);
+  return (n & 1) ? tuplell(d, c + d) : tuplell(c, d);
+}
+
+int main(int argc, char **argv) {
+  uint64_t N = 0;
+  cin >> N;
+  const auto [a, b] = fib(N);
+  cout << a << endl;
+  return 0;
 }
