dc2eade412ba8cc87f1f7d762670b56e818882cc
Rafael Henrique Tibães <r@fael.nl>
2018-09-11 12:38:14 -0300

    CSV

--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -1,10 +1,27 @@
-#include <iostream>
 #include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv)
-{
-	return 0;
+using namespace std;
+
+int main(int argc, char **argv) {
+  int N;
+  cin >> N;
+  while (N--) {
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string line;
+    getline(cin, line);
+
+    vector<string> tokens;
+    boost::split(tokens, line, boost::is_any_of(","));
+
+    // rtrim and ltrim would be usefull in the case of malformated inputs
+    cout << tokens[0] << " is " << tokens[4] << " years old and lives in "
+         << tokens[2] << ", " << tokens[1] << "." << endl;
+  }
+  return 0;
 }
