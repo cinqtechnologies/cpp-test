c27b646e97ebb73e6fa6f5053aa18b8cb24fca15
Rafael Henrique Tibães <r@fael.nl>
2018-09-11 13:08:53 -0300

    dirty_bills

--- challenges/dirty_bills/dirty_bills.cpp
+++ challenges/dirty_bills/dirty_bills.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
-{
-	return 0;
+using namespace std;
+
+int main(int argc, char **argv) {
+  int N = 0;
+  cin >> N;
+  int sum = 0;
+  while (N--) {
+    int a = 0;
+    string s;
+    cin >> a >> s;
+    if (s.compare("dirty") == 0) {
+      sum += a;
+    }
+  }
+  if (sum <= 0) {
+    cout << "There are no dirty bills." << endl;
+  } else {
+    cout << "There are " << sum << " dirty bills." << endl;
+  }
+  return 0;
 }
