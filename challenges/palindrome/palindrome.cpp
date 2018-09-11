19807a25d2920f68355101596b9fc30a03371720
Rafael Henrique Tibães <r@fael.nl>
2018-09-11 16:14:31 -0300

    Palindrome

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
-{
-	return 0;
+using namespace std;
+
+bool isPalindrome(const string &s) {
+  return equal(s.begin(), s.begin() + (s.size() / 2), s.rbegin());
+}
+
+int main(int argc, char **argv) {
+  int N = 0;
+  cin >> N;
+  while (N--) {
+    string w;
+    cin >> w;
+    cout << w << (isPalindrome(w) ? " is " : " is not ") << "palindrome"
+         << endl;
+  }
+  return 0;
 }
