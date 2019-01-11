840b3c509d0a2ec6a7fb0341a2c0bb76e1267fe7
Luiz Augusto Puglisi <lapuglisi@archlinux.local>
2019-01-11 11:49:31 -0200

    Challenge 'csv' solved, 'anagram.cpp' modified

--- challenges/anagram/anagram.cpp
+++ challenges/anagram/anagram.cpp
@@ -2,7 +2,61 @@
 #include <algorithm>
 #include <string>
 
+using namespace std;
+
+/*
+ * Function: is_anagram
+ *
+ * Checks whether the second string is an anagram of the first string
+ *
+ * Params:
+ *   first: first string
+ *   second: second string
+ *
+ * Returns:
+ *   true if the strings are anagrams, false otherwise
+ */
+bool is_anagram(const string& first, const string& second)
+{
+	/*
+	 * Not the fastest way but works with the test cases
+	 */
+	string s_one(first);
+	string s_two(second);
+
+	if (s_one.length() != s_two.length())
+	{
+		return false;
+	}
+
+	std::sort(s_one.begin(), s_one.end());
+	std::sort(s_two.begin(), s_two.end());
+
+	return (s_one.compare(s_two) == 0);
+}
+
 int main(int argc, char** argv)
 {
+	int queries = 0;
+	bool anagram = false;
+
+	string first_string, second_string;
+
+	cin >> queries;
+	while (queries > 0)
+	{
+		cin >> first_string;
+		cin >> second_string;
+
+		anagram = is_anagram(first_string, second_string);
+
+		cout << first_string << " "
+		     << (anagram ? "is" : "is not") << " "
+				 << "an anagram of " << second_string
+				 << endl;
+		
+		--queries;
+	}
+
 	return 0;
 }
--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -4,7 +4,100 @@
 #include <string>
 #include <vector>
 
+#include <string.h>
+
+using namespace std;
+
+#define CSV_ENTRY_DELIMITER	','
+#define CSV_FIELD_MAXLENGTH	256
+
+class CSVEntry
+{
+	public:
+		CSVEntry(){};
+
+		void parse_string(const string& source)
+		{
+			char csv_entry[CSV_FIELD_MAXLENGTH + 1];
+
+			stringstream ss(source);
+
+			_name.clear();
+			_city.clear();
+			_state.clear();
+			_country.clear();
+			_age = 0;
+
+			/* Retrieves the name */
+			memset(&(csv_entry[0]), CSV_FIELD_MAXLENGTH + 1, 0x00);
+			ss.getline(csv_entry, CSV_FIELD_MAXLENGTH, CSV_ENTRY_DELIMITER);
+			_name.assign(csv_entry);
+
+			/* Retrieves the state */
+			memset(&(csv_entry[0]), CSV_FIELD_MAXLENGTH + 1, 0x00);
+			ss.getline(csv_entry, CSV_FIELD_MAXLENGTH, CSV_ENTRY_DELIMITER);
+			_state.assign(csv_entry);
+
+			/* Retrieves the city */
+			memset(&(csv_entry[0]), CSV_FIELD_MAXLENGTH + 1, 0x00);
+			ss.getline(csv_entry, CSV_FIELD_MAXLENGTH, CSV_ENTRY_DELIMITER);
+			_city.assign(csv_entry);
+
+			/* Retrieves the country */
+			memset(&(csv_entry[0]), CSV_FIELD_MAXLENGTH + 1, 0x00);
+			ss.getline(csv_entry, CSV_FIELD_MAXLENGTH, CSV_ENTRY_DELIMITER);
+			_country.assign(csv_entry);
+
+			/* Retrieves the age */
+			memset(&(csv_entry[0]), CSV_FIELD_MAXLENGTH + 1, 0x00);
+			ss.getline(csv_entry, CSV_FIELD_MAXLENGTH, CSV_ENTRY_DELIMITER);
+			_age = atoi(csv_entry);
+		};
+
+		string to_string()
+		{
+			stringstream ss;
+
+			ss << _name << " is "
+			   << _age  << " years old "
+				 << "and lives in "
+				 << _city << ", " << _state << ".";
+
+			return ss.str();
+		};
+
+	private:
+		string _name;
+		string _city;
+		string _state;
+		string _country;
+		int _age;
+};
+
 int main(int argc, char** argv)
 {
+	int queries = 0;
+
+	string entry;
+
+	CSVEntry csv_entry;
+
+	cin >> queries;
+
+	/* Clears the stdin buffer */
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), 0x0a);
+	
+	while (queries > 0)
+	{
+		getline(cin, entry);
+
+		csv_entry.parse_string(entry);
+
+		cout << csv_entry.to_string() << endl;
+
+		--queries;
+	}
+
 	return 0;
 }
