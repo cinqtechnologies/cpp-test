45a29afb2362a982d8c8b05dcd2b820dd258fe31
Luiz Augusto Puglisi <lapuglisi@archlinux.local>
2019-01-11 12:39:18 -0200

    Challenge 'fibonacci' solved

--- challenges/fibonacci/fibonacci.cpp
+++ challenges/fibonacci/fibonacci.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+long int get_fibonacci(int index)
+{
+	long int first = 0, second = 1;
+
+	while (index > 1) /* or index >= 2 */
+	{
+		first = first + second;
+		second = second + first;
+
+		index -= 2;
+	}	
+
+	return (index % 2 == 0 ? first : second);
+}
+
 int main(int argc, char** argv)
 {
+	int index = 0;
+
+	std::cin >> index;
+
+	if (index >= 0)
+	{
+		std::cout << get_fibonacci(index) << std::endl;
+	}
+
 	return 0;
 }
