12d48b23669109846bee312d3a2e13c9aa2e7567
Luiz Augusto Puglisi <lapuglisi@archlinux.local>
2019-01-12 09:59:35 -0200

    Solved challenge 'palindrome'.

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
 #include <string>
 
+// Function: isPalindrome
+//
+// Checks whether a word is palindrome
+//
+// Params:
+//  word: the word to be checked
+//
+// Returns 
+//  true when 'word' is palindrome, false otherwise
+bool isPalindrome(const std::string& word)
+{
+	bool is_palindrome = false;
+	size_t front = 0L, back = word.length();
+
+	if (0L == back)
+	{
+		return false;
+	}
+
+	--back;
+	while (back >= front)
+	{
+		is_palindrome = (word[front] == word[back]);
+		if (!is_palindrome)
+		{
+			break;
+		}
+
+		front++;
+		back--;
+	}
+
+	return is_palindrome;
+}
+
 int main(int argc, char** argv)
 {
+	int entries = 0;
+	bool is_palindrome = false;
+
+	std::string word;
+
+	std::cin >> entries;
+
+	std::cin.ignore();
+	while (entries > 0)
+	{
+		std::cin >> word;
+
+		is_palindrome = isPalindrome(word);
+		std::cout << word  << " "
+		          << (is_palindrome ? "is" : "is not") << " "
+							<< "palindrome" 
+							<< std::endl;
+
+		--entries;
+	}
 	return 0;
 }
