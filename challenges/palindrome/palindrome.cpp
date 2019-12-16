55014318dc93a80429d5172de9117fe3e14ae5af
Jose Neto <netolcc06@gmail.com>
2019-12-15 19:32:30 -0800

    palindrome challenge completed

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include <string>
+#include <iterator>
+
+using namespace std;
+
+void isPalindrome(string word){
+		int begin = 0, end = word.length()-1;
+		while(begin < end){
+				if(word[begin]==word[end]){
+						begin++;
+						end--;
+				}
+				else{
+						cout << word << " is not palindrome" << endl;
+						return;
+				}
+		}
+		cout << word << " is palindrome" << endl;
+}
 
 int main(int argc, char** argv)
 {
-	return 0;
+		int numOfWords = 0;
+
+		cin >> numOfWords;
+
+		for(int i =0; i<numOfWords; i++){
+				string tmp;
+				cin >> tmp;
+				isPalindrome(tmp);
+		}
+
+		return 0;
 }
