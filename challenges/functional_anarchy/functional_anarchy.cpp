0a84663f4161434326173484eda9c10ccf023103
Jose Neto <netolcc06@gmail.com>
2019-12-15 16:41:21 -0800

    functional anarchy challenge completed

--- challenges/functional_anarchy/functional_anarchy.cpp
+++ challenges/functional_anarchy/functional_anarchy.cpp
@@ -4,7 +4,24 @@
 #include <iterator>
 #include <numeric>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+	int numOfIntegers = 0;
+	cin >> numOfIntegers;
+	vector<int> numbers;
+
+	for(int i=0; i<numOfIntegers;i++){
+			int numberToBeAdded;
+			cin >> numberToBeAdded;
+			numbers.push_back(numberToBeAdded);
+	}
+
+	sort(numbers.begin(), numbers.end());
+
+	for(auto it = numbers.begin(); it != numbers.end(); it++)
+			cout << *it << endl;
+
 	return 0;
 }
