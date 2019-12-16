890b0590fb154a30b8693738f0c9e0de941f684b
Jose Neto <netolcc06@gmail.com>
2019-12-15 19:56:00 -0800

    stairway to heaven challenge completed

--- challenges/stairway_to_heaven/stairway_to_heaven.cpp
+++ challenges/stairway_to_heaven/stairway_to_heaven.cpp
@@ -2,7 +2,33 @@
 #include <string>
 #include <iomanip>
 
+using namespace std;
+
+void printSpaces(int n){
+		for(int i =0; i < n; i++)
+				cout << " ";
+}
+
+void printStairs(int n){
+	for(int i =0; i < n; i++)
+			cout << "#";
+	cout << endl;
+}
+
 int main(int argc, char** argv)
 {
-	return 0;
+		int numberOfLevels = 0;
+
+		cin >> numberOfLevels;
+		if(numberOfLevels <=0){
+				cout << "Error." << endl;
+				return 0;
+		}
+
+		for(int i = numberOfLevels-1; i >=0; i--){
+				printSpaces(i);
+				printStairs(numberOfLevels-i);
+		}
+
+		return 0;
 }
