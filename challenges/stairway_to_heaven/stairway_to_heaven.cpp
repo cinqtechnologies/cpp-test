903defdb2acbed6e1e92d5a049bdae3a0f4657f2
Jhonatan da Rosa <jhonatandarosa@gmail.com>
2020-09-02 20:08:27 -0300

    stairway to heaven challenge

--- challenges/stairway_to_heaven/stairway_to_heaven.cpp
+++ challenges/stairway_to_heaven/stairway_to_heaven.cpp
@@ -2,7 +2,27 @@
 #include <string>
 #include <iomanip>
 
+#include <solver.hpp>
+
+void solution(std::istream& in, std::ostream& out) {
+    int N;
+    in >> N;
+
+    if (N < 1) {
+        out << "Error." << std::endl;
+        return;
+    }
+
+    // print the stairway
+    for (int i = 0; i < N; i++) {
+        out << std::setfill(' ') << std::setw(N - 1 - i) << "";
+        out << std::setfill('#') << std::setw(i+1) << '#';
+        out << std::endl;
+    }
+}
+
 int main(int argc, char** argv)
 {
-	return 0;
+    cpptest::solver::basic(argc, argv).execute(solution);
+    return 0;
 }
