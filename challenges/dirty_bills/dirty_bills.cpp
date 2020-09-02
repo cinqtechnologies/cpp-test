37bd1ac67c31ec1e549d4e4dcd82881aedcd6384
Jhonatan da Rosa <jhonatandarosa@gmail.com>
2020-09-02 20:19:20 -0300

    dirty bills challenge

--- challenges/dirty_bills/dirty_bills.cpp
+++ challenges/dirty_bills/dirty_bills.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include <string>
 
+#include <solver.hpp>
+
+void solution(std::istream& in, std::ostream& out) {
+    int N;
+    in >> N;
+
+    // here we'll count the bills
+    long long dirtyBills = 0;
+
+    int amount;
+    std::string state;
+
+    while (N-- > 0) {
+        in >> amount;
+        in >> state;
+
+        if (state == "dirty") {
+            dirtyBills += amount;
+        }
+    }
+
+    if (dirtyBills > 0) {
+        out << "There are " << dirtyBills <<" dirty bills." << std::endl;
+    } else {
+        out << "There are no dirty bills." << std::endl;
+    }
+
+}
+
 int main(int argc, char** argv)
 {
-	return 0;
+    cpptest::solver::basic(argc, argv).execute(solution);
+    return 0;
 }
