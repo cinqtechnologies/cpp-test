df89793e2b3a8b1e63ba698405b1ebeaf80c94bf
Jhonatan da Rosa <jhonatandarosa@gmail.com>
2020-09-03 15:06:19 -0300

    csv challenge

--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-#include <algorithm>
-#include <sstream>
 #include <string>
 #include <vector>
 
+#include <solver.hpp>
+
+
+const std::ctype<char>* csvSeparators() {
+    using char_ctype = std::ctype<char>;
+    static std::vector<char_ctype::mask> separators;
+    if (separators.empty()) {
+        const auto baseTable = char_ctype::classic_table();
+        std::vector<char_ctype::mask> customTable(baseTable, baseTable + char_ctype::table_size);
+        customTable[' '] ^= std::ctype_base::space;
+        customTable[','] ^= std::ctype_base::space;
+        separators.swap(customTable);
+    }
+
+    return new char_ctype(std::data(separators));;
+}
+
+void solution(std::istream& in, std::ostream& out) {
+    auto baseLoc = in.getloc();
+
+    in.imbue(std::locale(in.getloc(), csvSeparators()));
+
+    std::string name, state, city, country;
+    int age;
+
+    in >> name >> state >> city >> country >> age;
+    out << name << " is " << age << " years old"
+        << " and lives in " << city << ", " << state << "."
+        << std::endl;
+
+    in.imbue(baseLoc);
+}
+
 int main(int argc, char** argv)
 {
-	return 0;
+    cpptest::solver::codejam(argc, argv).execute(solution);
+    return 0;
 }
