36c2e696d04b61d31f151ccebc6b7abb214cfba9
Jhonatan da Rosa <jhonatandarosa@gmail.com>
2020-09-03 20:59:04 -0300

    inheritance challenge

--- challenges/inheritance/inheritance.cpp
+++ challenges/inheritance/inheritance.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <functional>
 
 class Animal
 {
+    static std::unordered_map<std::string, std::function<Animal*()>> registry;
+
+    template<typename T>
+    friend class AnimalRegistry;
+
 public:
 	/// This is the function which will be called to reproduce the sound each
 	/// animal makes.
@@ -19,12 +26,120 @@ public:
 };
 
 // Write your classes here
+std::unordered_map<std::string, std::function<Animal*()>> Animal::registry;
 
+template<typename AnimalType>
+Animal* animalCreator() {
+    return new AnimalType;
+}
+
+template <typename AnimalType>
+class AnimalRegistry {
+public:
+    AnimalRegistry(const std::string& name) {
+        Animal::registry.insert(std::make_pair(name, animalCreator<AnimalType>));
+    }
+};
+
+class Dog : public Animal {
+public:
+    static const AnimalRegistry<Dog> reg;
+
+    void makeSound() const override {
+        std::cout << "woof" << std::endl;
+    }
+};
+const AnimalRegistry<Dog> Dog::reg("dog");
+
+class Cat : public Animal {
+public:
+    static const AnimalRegistry<Cat> reg;
+    void makeSound() const override {
+        std::cout << "meow" << std::endl;
+    }
+};
+const AnimalRegistry<Cat> Cat::reg("cat");
+
+class Bird : public Animal {
+public:
+    static const AnimalRegistry<Bird> reg;
+    void makeSound() const override {
+        std::cout << "tweet" << std::endl;
+    }
+};
+const AnimalRegistry<Bird> Bird::reg("bird");
+
+class Mouse : public Animal {
+public:
+    static const AnimalRegistry<Mouse> reg;
+    void makeSound() const override {
+        std::cout << "squeek" << std::endl;
+    }
+};
+const AnimalRegistry<Mouse> Mouse::reg("mouse");
+
+class Cow : public Animal {
+public:
+    static const AnimalRegistry<Cow> reg;
+    void makeSound() const override {
+        std::cout << "moo" << std::endl;
+    }
+};
+const AnimalRegistry<Cow> Cow::reg("cow");
+
+class Frog : public Animal {
+public:
+    static const AnimalRegistry<Frog> reg;
+    void makeSound() const override {
+        std::cout << "croak" << std::endl;
+    }
+};
+const AnimalRegistry<Frog> Frog::reg("frog");
+
+class Elephant : public Animal {
+public:
+    static const AnimalRegistry<Elephant> reg;
+    void makeSound() const override {
+        std::cout << "toot" << std::endl;
+    }
+};
+const AnimalRegistry<Elephant> Elephant::reg("elephant");
+
+class Duck : public Animal {
+public:
+    static const AnimalRegistry<Duck> reg;
+    void makeSound() const override {
+        std::cout << "quack" << std::endl;
+    }
+};
+const AnimalRegistry<Duck> Duck::reg("duck");
+
+class Fish: public Animal {
+public:
+    static const AnimalRegistry<Fish> reg;
+    void makeSound() const override {
+        std::cout << "blub" << std::endl;
+    }
+};
+const AnimalRegistry<Fish> Fish::reg("fish");
+
+class Seal : public Animal {
+public:
+    static const AnimalRegistry<Seal> reg;
+    void makeSound() const override {
+        std::cout << "ow ow ow" << std::endl;
+    }
+};
+const AnimalRegistry<Seal> Seal::reg("seal");
 // This is the function you need to change in order to instantiate the required
 // classes.
 Animal* Animal::create(const std::string& name)
 {
-	return 0;
+    const auto it = registry.find(name);
+    if (it == registry.end()) {
+        return nullptr;
+    }
+    return it->second();
 }
 
 void makeSound(const std::string& name)
